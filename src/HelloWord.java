46f3f02a6e7ef8837ee5d93bdbb7caa20955f36b
Tomek <draganiktomasz@gmail.com>
2019-12-26 17:08:30 +0100

    pierwsze linijki

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -1,5 +1,19 @@
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("Hello Word");
+
+        String firstName = "Jan";
+        String lastName = "Kowalski";
+        int age = 25;
+        double przyrost = 10;
+        double height = 150.7;
+        double lacznyWzrost = przyrost + height;
+        final String pesel = "4234242423" ;
+
+
+
+        System.out.println( firstName + " " + lastName +" ma 26 lt");
+        System.out.println("Pan " + firstName + " "+ lastName +" " + "Jan Kowaski ma PESEL: " + pesel);
+        System.out.println("Jan na wzrostu: " + height);
+        System.out.println("Jeżeli Jan urośnie jeszcze o " + przyrost+ " to będzie miał: " + (height + przyrost) + " cm");
     }
 }
