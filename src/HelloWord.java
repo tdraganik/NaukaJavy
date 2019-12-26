e178cf17efde0d1d702797c73f57a25640b57827
Tomek <draganiktomasz@gmail.com>
2019-12-26 15:47:43 +0100

    właściwy commit

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -1,14 +1,5 @@
-import java.sql.SQLOutput;
-
 public class HelloWord {
     public static void main(String[] args) {
-
-            System.out.println("Mam Audi A4");
-            System.out.println("Rocznik:");
-            System.out.println(2004);
-            System.out.println("Silnik o pojemności:");
-            System.out.println(2.0);
-        }
+        System.out.println("Hello Word");
     }
-
-
+}
